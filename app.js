4474e72bc002a08c1e96fe8b9c1f3982f368d770
Abundnce10 <michaelstitt@gmail.com>
2014-01-29 23:10:46 -0800

    Show random question and detect whether user's answer is correct

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	app = express(),
-	server = require('http').createServer(app);
+	server = require('http').createServer(app),
+	questions = require('./JeopardyQuestions2012.json');
 
 
 server.listen(process.env.PORT || 3000);
@@ -8,4 +9,17 @@ server.listen(process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/index.html');
-})
+});
+
+
+// return a random question
+app.get('/question', function(req, res) {
+	res.json(questions[randomQuestion(0,questions.length)]);
+});
+
+
+
+function randomQuestion(min, max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
